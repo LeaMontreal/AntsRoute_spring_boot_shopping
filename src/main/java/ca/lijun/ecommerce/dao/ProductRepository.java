e8836f08d564c44342d05db741948e6e8c451510
lilianwang <wanglijun00123172@gmail.com>
2022-08-12 21:50:49 -0400

    V0.4 search products by keyword

--- src/main/java/ca/lijun/ecommerce/dao/ProductRepository.java
+++ src/main/java/ca/lijun/ecommerce/dao/ProductRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
   // query method, Spring will execute a sql query
   Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+  Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
