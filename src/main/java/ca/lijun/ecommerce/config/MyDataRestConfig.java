a6e7dab08bd389f8fdb379c85ec113c1b01d6247
lilianwang <wanglijun00123172@gmail.com>
2022-08-12 22:36:56 -0400

    V0.5 add api for country and state

--- src/main/java/ca/lijun/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/ca/lijun/ecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package ca.lijun.ecommerce.config;
 
+import ca.lijun.ecommerce.entity.Country;
 import ca.lijun.ecommerce.entity.Product;
 import ca.lijun.ecommerce.entity.ProductCategory;
+import ca.lijun.ecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -26,20 +28,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-    // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-    config.getExposureConfiguration()
-            .forDomainType(Product.class)
-            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-    // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-    config.getExposureConfiguration()
-            .forDomainType(ProductCategory.class)
-            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    // disable HTTP methods: PUT, POST, DELETE and PATCH
+    disableHttpMethods(Product.class, config, theUnsupportedActions);
+    disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+    disableHttpMethods(Country.class, config, theUnsupportedActions);
+    disableHttpMethods(State.class, config, theUnsupportedActions);
 
     // call an internal helper method
     exposeIds(config);
+  }
+
+  // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+  private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+    config.getExposureConfiguration()
+            .forDomainType(theClass)
+            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
   }
 
   // expose entity ids
---
+++ src/main/java/ca/lijun/ecommerce/entity/Country.java
@@ -0,0 +1,28 @@
+package ca.lijun.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Data
+public class Country {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="id")
+  private int id;
+
+  @Column(name="code")
+  private String code;
+
+  @Column(name="name")
+  private String name;
+
+  @OneToMany(mappedBy = "country")
+  @JsonIgnore
+  private List<State> states;
+
+}
